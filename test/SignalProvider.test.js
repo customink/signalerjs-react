a0b74b362c472bd8267b4a80f5187aea333c0500
Alex Egan <alexander.egan@customink.com>
2017-09-21 11:26:32 -0400

    Move tests to jest instead of mocha
    We're using expect js in this project, which is now part of jest. The team has
    decided to move to jest in this project instead of replacing expect.

--- test/SignalProvider.test.js
+++ test/SignalProvider.test.js
@@ -1,33 +1,36 @@
 import React from 'react';
 import {shallow, mount} from 'enzyme';
-import expect from 'expect';
 import {SignalProvider} from '../src';
 
 describe('SignalProvider', () => {
   const features = {some: 'features'};
   const signals = {foo: 'bar'};
-  it('puts things in the context', () => {
+
+  test('puts things in the context', () => {
     const c = mount(<SignalProvider features={features}><div/></SignalProvider>);
     c.instance().signals = true;
-    expect(c.instance().getChildContext()).toMatch({
+    expect(c.instance().getChildContext()).toMatchObject({
       signals: true,
       updateSignals: c.instance().updateSignals
     });
   });
-  it('sets signals', () => {
+
+  test('sets signals', () => {
     const c = shallow(<SignalProvider features={features}><div/></SignalProvider>);
     const p = Promise.resolve(signals);
     c.instance().signaler = {featureFlags() {return p;}};
     c.instance().componentDidMount();
     return p.then(() => expect(c.instance().signals).toBe(signals));
   });
-  it('conditionally renders children', () => {
+
+  test('conditionally renders children', () => {
     const c = shallow(<SignalProvider features={features}><div/></SignalProvider>);
     expect(c.find('div').length).toBe(0);
     c.instance().updateSignals({showStuff: true});
     expect(c.find('div').length).toBe(1);
   });
-  it('renders function', () => {
+
+  test('renders function', () => {
     const child = signals => {
       return signals && <div>hi!</div>;
     };
